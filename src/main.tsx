fa483a47b62a67273d4ba735cd72ee0f8e8e96d3
amanda <amandahhhimler4@gmail.com>
2023-08-29 21:42:03 -0300

    estrutura pronta

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,18 @@ import './index.css'
 //components
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Header/>
-    <Footer/>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        {/* <Route path='NomeDaRota' element={<NomeDoComponente/>}/> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+
+
   </React.StrictMode>,
 )
