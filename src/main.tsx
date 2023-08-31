cc1c679450436b2c067e47f9da54031ce7f5ecb1
amanda <amandahhhimler4@gmail.com>
2023-08-30 21:49:01 -0300

    atualizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+
+
+//componentes
+import Home from "./pages/PerfilUsuario/";
+
+
+//estilização global
+import "./index.css";
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
