a1b420195efab802ef3b7f2e9389fb6bf92fcdef
Nicolas <nicolashenrisilva@outlook.com>
2023-08-30 21:40:33 -0300

    tsx da pagina feed feita. css pela metade

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 //components
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Feed from './pages/Feed'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,10 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Header />
       <Routes>
         {/* <Route path='NomeDaRota' element={<NomeDoComponente/>}/> */}
+        <Route path='/feed' element={<Feed/>}/>
       </Routes>
       <Footer />
     </BrowserRouter>
 
 
-  </React.StrictMode>,
+  </React.StrictMode>
 )
