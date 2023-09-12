c0906b945d3a9c0989642d8ccf3f189cc870eee5
Flamarion <flamarionsouza23@gmail.com.br>
2023-09-12 20:40:44 -0300

    componetizacao filmes Informaçoes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+// componentes
+import Header from './components/Header'
+import FilmesInformacoes from './pages/FilmesInformacoes'
+import Footer from './components/Footer'
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+<BrowserRouter>
+<Header/>
+<Routes>
+<Route path='/filmesInformacoes' element={<FilmesInformacoes/>}/> */
+</Routes>
+<Footer/>
+</BrowserRouter>
   </React.StrictMode>,
 )
