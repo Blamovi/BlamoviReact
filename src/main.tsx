1876bf9f9236b962e93806e6565403012c1c6828
Flamarion <flamarionsouza23@gmail.com.br>
2023-09-13 21:36:41 -0300

    refino2 componentização calendario

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+// componentes
+import Header from './components/Header'
+import Calendario from './pages/Calendario'
+import Footer from './components/Footer'
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+<BrowserRouter>
+<Header/>
+<Routes>
+<Route path='/Calendario' element={<Calendario/>}/> */
+</Routes>
+<Footer/>
+</BrowserRouter>
   </React.StrictMode>,
 )
