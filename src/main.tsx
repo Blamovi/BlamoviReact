5f7fc735e7a3aa94fa07d1c7e0c2210cd674baf3
Flamarion <flamarionsouza23@gmail.com.br>
2023-09-19 20:20:32 -0300

    atualizacao5

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,16 @@ import Header from './components/Header'
 import FilmesInformacoes from './pages/FilmesInformacoes'
 import Footer from './components/Footer'
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<BrowserRouter>
-<Header/>
-<Routes>
-<Route path='/filmesInformacoes' element={<FilmesInformacoes/>}/> */
-</Routes>
-<Footer/>
-</BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/filmesInformacoes' element={<FilmesInformacoes />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/filmesInformacoes/index.tsx
+++ src/pages/filmesInformacoes/index.tsx
@@ -318,18 +318,21 @@ function FilmesInformacoes() {
                         </div>
                     </div>
                     <div className="Comentarios_todos">
-                        {
-                            comentarioFilmes.map((comentarFilme: any) => {
-                                return <div key={comentarFilme.id}>
-                                    <Comentario
-                                        nome={comentarFilme.nome}
-                                        foto={comentarFilme.foto}
-                                        comentarios={comentarFilme.comentario}
-                                    />
-                                </div>
-                            })
-                        }
+                        <div className="img_coment">
+                            <img
+                                className="ricardo"
+                                src={imgRicardo}
+                                alt=""
+                            />
+                        </div>
                         <div>
+                            <p className="usuario2">Ricardo Motta</p>
+                            <p className="comentario_texto">
+                            APLAUDO de pé essa obra extraordinária. Christopher Nolan surpreende
+                            com suas ideias e criatividade de cinema desde essa época, assim como
+                            a participação de David Goyer no roteiro. A atuação de Christian Bale
+                            é IMPECÁVEL, esse Homem não tem defeitos.
+                            </p>
                             <img
                                 className="like"
                                 src={imglike}
@@ -370,9 +373,8 @@ function FilmesInformacoes() {
                     </div>
                 </section>
             </main>
-
         </>
-    )
+    );
 }
 
-export default FilmesInformacoes
+export default FilmesInformacoes;
