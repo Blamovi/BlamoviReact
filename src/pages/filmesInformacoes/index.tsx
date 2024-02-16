69552369427899a0dbedaac90d0e9ca632d1b22e
Flamarion Souza <flamarionsouza23@gmail.com>
2024-02-15 22:50:23 -0300

    up date 4

--- src/pages/filmesInformacoes/index.tsx
+++ src/pages/filmesInformacoes/index.tsx
@@ -1,5 +1,6 @@
 import "./style.css"
-import { Link, useParams, useNavigate } from "react-router-dom";
+
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "../../utils/api";
 
